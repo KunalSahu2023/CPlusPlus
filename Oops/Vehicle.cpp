cf3c20ed2e9cf94c45d91b43f6485606318a07ca
Kunal Sahu <kunalsahu3131@gmail.com>
2025-04-22 20:37:25 +0530

    ..

--- Oops/Vehicle.cpp
+++ Oops/Vehicle.cpp
@@ -1,14 +1,40 @@
 
-// creating class
 
 #include <iostream>
 using namespace std;
 
-class vehicle
+// creating class
+
+class Vehicle // Vehicle is class variable or class name
 {
+
+public: // Public is a Access specifier or modifier
+    // class member :
     int noOfGear;
     string brand;
     string model;
     int airBag;
     string color;
+
+    // class member function
+    void display()
+    {
+        cout << "Details of Car : " << endl;
+        cout << "Brand Name :" << brand << endl;
+        cout << "Model N0. : " << model << endl;
+        cout << "No of Airbag : " << airBag << endl;
+        cout << "No of Gear : " << noOfGear << endl;
+    }
 };
+
+int main()
+{
+    // Creating Object
+
+    Vehicle car;          // Car is a object of class Vehicle which is used to access properties of Template class.
+    car.brand = "Toyota"; // Dot (.) to access properties of class we use dot along with object to use class's properties
+    car.model = "T140202";
+    car.airBag = 4;
+    car.noOfGear = 4;
+    car.display(); // display : it is a method of class which dispaly data of class member.
+}
