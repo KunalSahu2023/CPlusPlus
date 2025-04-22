cb308d3aef0e6c3076314f6fcc2971529f99d66f
Kunal Sahu <kunalsahu3131@gmail.com>
2025-04-22 20:26:12 +0530

    creating class

---
+++ Oops/Vehicle.cpp
@@ -0,0 +1,14 @@
+
+// creating class
+
+#include <iostream>
+using namespace std;
+
+class vehicle
+{
+    int noOfGear;
+    string brand;
+    string model;
+    int airBag;
+    string color;
+};
